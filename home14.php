6c85f593bde55603bb15f44186ce2caee3cb455b
Sérgio Raphael A. Sampaio <67436514+Svetrar@users.noreply.github.com>
2021-12-21 20:25:54 -0300

    Update and rename SQL.php to home14.php

---
+++ home14.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <body>       
+		<a href="home14.php">Home</a><br>
+		<a href="alterar14.php">Alterar Aluno</a><br>
+		<a href="listar14.php">Listar Alunos</a><br>
+		<a href="matriculaAlerar.php">Excluir Aluno</a><br>
+		<a href="matriculaListar.php">Detalhe de Aluno</a><br>
+ 
+        <br><br>
+		
+    </body>
+</html>
