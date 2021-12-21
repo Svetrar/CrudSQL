731a7d74987649f5d04e2ef4e4e696849fc19c7c
Sérgio Raphael A. Sampaio <67436514+Svetrar@users.noreply.github.com>
2021-12-21 20:30:08 -0300

    Update home14.php

--- home14.php
+++ home14.php
@@ -5,8 +5,8 @@
 		<a href="home14.php">Home</a><br>
 		<a href="alterar14.php">Alterar Aluno</a><br>
 		<a href="listar14.php">Listar Alunos</a><br>
-		<a href="matriculaAlerar.php">Excluir Aluno</a><br>
-		<a href="matriculaListar.php">Detalhe de Aluno</a><br>
+		<a href="matriculaAlerar14.php">Excluir Aluno</a><br>
+		<a href="matriculaListar14.php">Detalhe de Aluno</a><br>
  
         <br><br>
 		
